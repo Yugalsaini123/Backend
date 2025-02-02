b872923dc072977b8c639d91915306d9fb7ec3f2
Yugal Saini <yugalsaini2002@gmail.com>
2025-02-02 22:29:37 +0530

    IPV4 access enabling

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ const startServer = async () => {
   try {
     await connectDB();
     // Redis connects automatically through ioredis
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       logger.info(`Server running on port ${PORT}`);
     });
   } catch (error) {
