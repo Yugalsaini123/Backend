1d72bb3e19e9d05aa80f4a8116ce4ce5b9905af2
Yugal Saini <yugalsaini2002@gmail.com>
2025-02-03 16:15:08 +0530

    cors new update1

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production' 
     ? 'https://faqtranslate.netlify.app' 
-    : ['http://localhost:3000', 'http://localhost:5173'],
+    : ['http://localhost:3000', 'http://localhost:5173', 'https://faqtranslate.netlify.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
